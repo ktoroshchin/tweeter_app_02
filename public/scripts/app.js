5645abd3a751a64c75f3f4073877eee60075be25
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-12 19:39:26 +0000

    Installed moment.js for timeStamp⏲

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     var $username = $("<h1>").text(tweet.user.name).addClass('user-name');
     var $idName = $("<p>").text(tweet.user.handle).addClass('handle');
 
-    var $timeAgo = $("<span>").text(timeSince(tweet.created_at));
+    var $timeAgo = $("<span>").text(moment(tweet.created_at).fromNow());
     var $icons = $("<span>").addClass('icons');
     var $iconFlag = $("<i>").addClass('fas fa-flag');
     var $iconRecycle = $("<i>").addClass('fas fa-recycle');
@@ -71,31 +71,4 @@ $(document).ready(function() {
       renderTweets(arrayOfTweets);
     });
   };
-
-
-function timeSince(date) {
-  var seconds = Math.floor((new Date() - date) / 1000);
-  var interval = Math.floor(seconds / 31536000);
-
-  if (interval > 1) {
-    return interval + " years";
-  }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months";
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-  }
 });
